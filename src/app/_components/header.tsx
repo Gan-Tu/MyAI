b5242da90eaff4768fccc02367d8105bd48627dd
Gan Tu <dev@tugan.io>
2024-12-25 11:55:19 -0500

    fix types for header

--- src/app/_components/header.tsx
+++ src/app/_components/header.tsx
@@ -1,5 +1,10 @@
 
-export default function Header({ title, subtitle }) {
+interface HeaderProps {
+  title?: string;
+  subtitle?: string;
+}
+
+export default function Header({ title, subtitle }: HeaderProps) {
   return (
     <div className="flex items-center p-6 border-b border-gray-200">
       <div className="">
