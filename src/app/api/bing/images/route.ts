3739519bccd7afac858a198dd25fe353733d07f2
Gan Tu <dev@tugan.io>
2024-12-26 21:47:06 -0500

    add more individual apis

---
+++ src/app/api/bing/images/route.ts
@@ -0,0 +1,21 @@
+import bing_search from '@/lib/agents';
+import { NextResponse, type NextRequest } from 'next/server';
+
+export async function GET(request: NextRequest) {
+  const searchParams = request.nextUrl.searchParams
+  const query = searchParams.get('query');
+  const count = Number(searchParams.get('count')) || 10;
+
+  if (!query) {
+    return NextResponse.json({ error: 'Missing query parameter' }, { status: 400 })
+  } else if (!count || count <= 0) {
+    return NextResponse.json({ error: 'Missing positive count parameter' }, { status: 400 })
+  }
+
+  try {
+    const searchResults = await bing_search(query, count);
+    return NextResponse.json(searchResults.images)
+  } catch (error) {
+    return NextResponse.json({ error: JSON.stringify(error) }, { status: 500 })
+  }
+}
---
+++ src/app/api/google/images/route.ts
@@ -0,0 +1,25 @@
+import { NextResponse, type NextRequest } from 'next/server';
+
+export async function GET(request: NextRequest) {
+  const searchParams = request.nextUrl.searchParams
+  const query = searchParams.get('query');;
+
+  if (!query) {
+    return NextResponse.json({ error: 'Missing query parameter' }, { status: 400 })
+  }
+
+  try {
+    const response = await fetch(
+      `https://www.googleapis.com/customsearch/v1?q=${query.trim()}&cx=${process
+        .env.GOOGLE_SEARCH_ENGINE_ID!}&key=${process.env
+          .GOOGLE_SEARCH_API_KEY!}&searchType=image&num=10`
+    );
+    if (!response.ok) {
+      return NextResponse.json({ error: response.status }, { status: 500 })
+    }
+    const { items } = await response.json();
+    return NextResponse.json({ data: items })
+  } catch (error) {
+    return NextResponse.json({ error: JSON.stringify(error) }, { status: 500 })
+  }
+}
