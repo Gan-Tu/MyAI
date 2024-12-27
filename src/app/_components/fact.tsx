2a9bd6fee9738c6faa5481e4e8b99a553d78d4e4
Gan Tu <dev@tugan.io>
2024-12-27 18:23:47 -0500

    Fix style of facts

--- src/app/_components/fact.tsx
+++ src/app/_components/fact.tsx
@@ -52,21 +52,21 @@ export default function Fact({
             setIsExpanded(!isExpanded);
           }
         }}
-        className={`${className} transition-all ease-in-out duration-200 justify-start  rounded-xl ${
+        className={`${className} transition-all ease-in-out duration-200 justify-start rounded-xl flex flex-col items-start ${
           isExpanded ? "col-span-full p-2" : "bg-blue-100 p-4"
-        } grid content-start`}
+        }`}
       >
         {/* Header */}
-        <motion.div className="flex justify-between items-start transition-all ease-in-out duration-200">
+        <motion.div className="flex min-w-full justify-between transition-all ease-in-out duration-200">
           <h2
             className={`text-sm font-semibold text-gray-900 text-left ${
-              isExpanded ? "text-[1rem]" : "text-base"
+              isExpanded ? "text-[1.125rem]" : "text-base"
             }`}
           >
             {name || <Loader className="min-h-6 w-[100px]" />}
           </h2>
           <ChevronDownIcon
-            className={`justify-end transition-all ease-in-out duration-200 ${
+            className={`transition-all ease-in-out duration-200 ${
               isExpanded
                 ? "w-7 h-7 rounded-xl p-1 bg-blue-100 rotate-180"
                 : "w-5 h-5"
@@ -74,40 +74,38 @@ export default function Fact({
           />
         </motion.div>
 
-        <motion.div className="flex flex-col justify-start items-start text-left align-middle transition-all ease-in-out duration-200">
-          {/* Short Answer */}
-          {!isExpanded && (
-            <motion.span
-              className={`mt-1 font-normal text-sm text-left transition-all ease-in-out duration-200 ${
-                isExpanded ? "" : "line-clamp-2"
-              }`}
-            >
-              {shortAnswer ? (
-                <MemoizedMarkdown id={`${name}-short`} content={shortAnswer} />
-              ) : (
-                <Loader className="min-h-6 w-[150px]" />
-              )}
-            </motion.span>
-          )}
+        {/* Short Answer */}
+        {!isExpanded && (
+          <motion.span
+            className={`mt-1 font-normal text-sm text-left transition-all ease-in-out duration-200 text-pretty ${
+              isExpanded ? "" : "line-clamp-2"
+            }`}
+          >
+            {shortAnswer ? (
+              <MemoizedMarkdown id={`${name}-short`} content={shortAnswer} />
+            ) : (
+              <Loader className="min-h-6 w-[150px]" />
+            )}
+          </motion.span>
+        )}
 
-          {/* Expandable Full Answer */}
-          {fullAnswer && (
-            <motion.div
-              ref={contentRef}
-              className="overflow-hidden transition-all ease-in-out duration-200 text-pretty"
-              style={{
-                maxHeight: contentHeight ? `${contentHeight}px` : "0px"
-              }}
-            >
-              <motion.span className="mt-1 text-sm font-normal">
-                <MemoizedMarkdown
-                  id={`${name}-full`}
-                  content={fullAnswer?.replace("* ", "  * ")}
-                />
-              </motion.span>
-            </motion.div>
-          )}
-        </motion.div>
+        {/* Expandable Full Answer */}
+        {fullAnswer && (
+          <motion.div
+            ref={contentRef}
+            className="overflow-hidden transition-all text-left ease-in-out duration-200 text-pretty"
+            style={{
+              maxHeight: contentHeight ? `${contentHeight}px` : "0px"
+            }}
+          >
+            <motion.span className="mt-1 text-sm font-normal">
+              <MemoizedMarkdown
+                id={`${name}-full`}
+                content={fullAnswer?.replace("* ", "  * ")}
+              />
+            </motion.span>
+          </motion.div>
+        )}
       </motion.button>
     </AnimatePresence>
   );
