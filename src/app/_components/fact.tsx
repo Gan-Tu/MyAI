10b164aeea09cbd3d7c86679065a5d17035bf63d
Gan Tu <dev@tugan.io>
2024-12-27 14:40:25 -0500

    update font size on expadned fact

--- src/app/_components/fact.tsx
+++ src/app/_components/fact.tsx
@@ -53,20 +53,15 @@ export default function Fact({
           setIsExpanded(!isExpanded);
         }
       }}
-      // index === 0
-      //   ? "bg-blue-100" // border border-blue-200
-      //   : index === 1
-      //   ? "bg-lime-100" // border border-green-200
-      //   : "bg-amber-100" // border border-yellow-200
       className={`${className} transition-all justify-start ease-in-out duration-300 rounded-xl ${
         isExpanded ? "col-span-full p-2" : "bg-blue-100 p-4"
       } grid content-start`}
     >
       {/* Header */}
-      <div className="flex justify-between items-start  transition-all ease-in-out duration-300">
+      <div className="flex justify-between items-start transition-all ease-in-out duration-300">
         <h2
           className={`text-sm font-semibold text-gray-900 text-left ${
-            isExpanded ? "text-lg" : "text-md"
+            isExpanded ? "text-[1rem]" : "text-base"
           }`}
         >
           {name || (
