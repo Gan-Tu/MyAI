f5afd1d135e4b7a2a8a46311e3c3c8eb01e7e83c
Gan Tu <dev@tugan.io>
2024-12-28 13:28:47 -0500

    fix font

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,10 +1,23 @@
 import { ApplicationLayout } from "@/components/layout/application-layout";
+import clsx from "clsx";
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
+import localFont from "next/font/local";
 import { Toaster } from "react-hot-toast";
 import "./globals.css";
 
-const inter = Inter({ subsets: ["latin"] });
+const inter = Inter({
+  subsets: ["latin"],
+  display: "swap",
+  variable: "--font-inter"
+});
+
+const monaSans = localFont({
+  src: "../fonts/Mona-Sans.var.woff2",
+  display: "swap",
+  variable: "--font-mona-sans",
+  weight: "200 900"
+});
 
 export const metadata: Metadata = {
   title: "AI Knowledge Card",
@@ -17,8 +30,11 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body className={`${inter.className} bg-muted/50`}>
+    <html
+      lang="en"
+      className={clsx("h-full antialiased", inter.variable, monaSans.variable)}
+    >
+      <body className="bg-muted/50">
         <Toaster position="top-right" />
         <main className="flex h-100vh flex-1 flex-col">
           <ApplicationLayout>{children}</ApplicationLayout>
