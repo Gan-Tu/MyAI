d6ebb5443264ece1856fe01059b1e5a96914ee56
Gan Tu <dev@tugan.io>
2024-12-30 13:16:01 -0500

    add create account and pwd reset form

--- src/app/login/email-form.tsx
+++ src/app/login/email-form.tsx
@@ -1,4 +1,5 @@
 import { Button } from "@/components/base/button";
+import { Checkbox } from "@/components/base/checkbox";
 import {
   Dialog,
   DialogActions,
@@ -6,10 +7,11 @@ import {
   DialogDescription,
   DialogTitle,
 } from "@/components/base/dialog";
-import { Field, Label } from "@/components/base/fieldset";
+import { ErrorMessage, Field, Label } from "@/components/base/fieldset";
 import { Input } from "@/components/base/input";
-import { loginWithEmail } from "@/lib/session";
-import { useState } from "react";
+import { createAccount, loginWithEmail, resetPassword } from "@/lib/session";
+import { CheckIcon } from "@heroicons/react/20/solid";
+import { useEffect, useState } from "react";
 import toast from "react-hot-toast";
 
 interface EmailFormProps {
@@ -17,9 +19,34 @@ interface EmailFormProps {
   setIsOpen: (isOpen: boolean) => void;
 }
 
+const validatePassword = (pwd: string) => {
+  if (pwd.length === 0) {
+    return "";
+  }
+  if (pwd.length < 8) {
+    return "Must be at least 8 characters long.";
+  }
+  if (!/[a-z]/.test(pwd)) {
+    return "Must contain at least one lowercase letter.";
+  }
+  if (!/\d/.test(pwd)) {
+    return "Must contain at least one number.";
+  }
+  return ""; // Valid password
+};
+
 export function EmailForm({ isOpen, setIsOpen }: EmailFormProps) {
   const [email, setEmail] = useState<string>("");
   const [password, setPassword] = useState<string>("");
+  const [rememberMe, setRememberMe] = useState<boolean>(true);
+  const [isCreateAccount, setIsCreateAccount] = useState<boolean>(false);
+  const [pwdError, setPwdError] = useState<string>("");
+
+  useEffect(() => {
+    if (isCreateAccount && password) {
+      setPwdError(validatePassword(password));
+    }
+  }, [isCreateAccount, password]);
 
   const onClick = async () => {
     if (!email) {
@@ -27,18 +54,50 @@ export function EmailForm({ isOpen, setIsOpen }: EmailFormProps) {
     } else if (!password) {
       toast.error("Password is required");
     } else {
-      const { error } = await loginWithEmail(email, password);
+      const { error } = isCreateAccount
+        ? await createAccount(email, password, rememberMe)
+        : await loginWithEmail(email, password, rememberMe);
       if (!error) {
         setIsOpen(false);
       }
     }
   };
 
+  const resetState = () => {
+    setEmail("");
+    setPassword("");
+    setRememberMe(true);
+    setPwdError("");
+  };
+
+  const onClose = () => {
+    setIsOpen(false);
+    resetState();
+  };
+
+  const toggleMode = () => {
+    setIsCreateAccount((x) => !x);
+    resetState();
+  };
+
+  const onForgetPassword = async () => {
+    if (!email) {
+      toast.error("Email is required");
+    } else {
+      const { error } = await resetPassword(email);
+      if (error) {
+        toast.error(error);
+      }
+    }
+  };
+
   return (
-    <Dialog size="md" open={isOpen} onClose={setIsOpen}>
-      <DialogTitle>Sign In</DialogTitle>
+    <Dialog size="sm" open={isOpen} onClose={onClose}>
+      <DialogTitle>{isCreateAccount ? "Sign Up" : "Sign In"}</DialogTitle>
       <DialogDescription>
-        Sign in to your account to continue.
+        {isCreateAccount
+          ? "Create an acconut to continue."
+          : "Sign in to your account to continue."}
       </DialogDescription>
       <DialogBody>
         <Field className="space-y-3">
@@ -53,24 +112,74 @@ export function EmailForm({ isOpen, setIsOpen }: EmailFormProps) {
             onChange={(e) => setEmail(e.target.value)}
           />
         </Field>
-        <Field className="mt-4 space-y-3">
-          <Label className="text-sm/5 font-medium dark:text-zinc-950">
-            Password
-          </Label>
-          <Input
-            required
-            type="password"
-            name="password"
-            value={password}
-            onChange={(e) => setPassword(e.target.value)}
-          />
-        </Field>
+        {(isCreateAccount || email) && (
+          <Field className="mt-4 space-y-3">
+            <Label className="text-sm/5 font-medium dark:text-zinc-950">
+              Password
+            </Label>
+            <Input
+              required
+              type="password"
+              name="password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+            />
+            {pwdError && <ErrorMessage>{pwdError}</ErrorMessage>}
+          </Field>
+        )}
+        <div className="mt-8 flex items-center justify-between text-sm/5">
+          <Field className="flex items-center gap-3">
+            <Checkbox
+              name="remember-me"
+              checked={rememberMe}
+              defaultChecked={true}
+              onChange={(checked) => setRememberMe(checked)}
+            >
+              <CheckIcon className="fill-white opacity-0 group-data-[checked]:opacity-100" />
+            </Checkbox>
+            <Label>Remember me</Label>
+          </Field>
+          {!isCreateAccount && email && (
+            <span
+              className="cursor-pointer font-medium hover:text-gray-600"
+              onClick={onForgetPassword}
+            >
+              Forgot password?
+            </span>
+          )}
+        </div>
       </DialogBody>
+      {!isCreateAccount ? (
+        <div className="mt-6 rounded-lg bg-gray-50 py-4 text-center text-sm/5 tracking-normal ring-1 ring-black/5">
+          Not a member?{" "}
+          <span
+            className="cursor-pointer font-medium hover:text-gray-600"
+            onClick={toggleMode}
+          >
+            Create an account
+          </span>
+        </div>
+      ) : (
+        <div className="mt-6 rounded-lg bg-gray-50 py-4 text-center text-sm/5 tracking-normal ring-1 ring-black/5">
+          Already an member?{" "}
+          <span
+            className="cursor-pointer font-medium hover:text-gray-600"
+            onClick={toggleMode}
+          >
+            Log in here.
+          </span>
+        </div>
+      )}
       <DialogActions>
-        <Button plain onClick={() => setIsOpen(false)}>
+        <Button plain onClick={onClose}>
           Cancel
         </Button>
-        <Button onClick={onClick}>Sign In</Button>
+        <Button
+          onClick={onClick}
+          disabled={!email || !password || pwdError.length > 0}
+        >
+          {isCreateAccount ? "Sign Up" : "Sign In"}
+        </Button>
       </DialogActions>
     </Dialog>
   );
--- src/lib/session.ts
+++ src/lib/session.ts
@@ -1,6 +1,11 @@
 import { auth } from "@/lib/firebase/client";
 import {
   AuthProvider,
+  browserLocalPersistence,
+  browserSessionPersistence,
+  createUserWithEmailAndPassword,
+  sendPasswordResetEmail,
+  setPersistence,
   signInWithEmailAndPassword,
   signInWithPopup,
   User
@@ -13,40 +18,64 @@ export enum LoginOption {
   EMAIL = "Email",
 }
 
-interface LoginResponse {
-  success: boolean;
+interface AuthResponse {
   user?: User
   error?: string;
 }
 
-export async function loginWithProvider(provider: AuthProvider): Promise<LoginResponse> {
+export async function loginWithProvider(provider: AuthProvider): Promise<AuthResponse> {
   try {
     const result = await signInWithPopup(auth, provider);
     const user = result.user;
     console.log(`Signed in user: ${user.displayName || user.email}`);
     toast.success("Successfully signed in!")
-    return { success: true, user };
+    return { user };
   } catch (error: any) {
     toast.error(`Failed to sign in: ${error.message}`);
-    return {
-      success: false,
-      error: error.message,
-    };
+    return { error: error.message };
   }
 }
 
-export async function loginWithEmail(email: string, password: string): Promise<LoginResponse> {
+export async function loginWithEmail(email: string, password: string, rememberMe: boolean): Promise<AuthResponse> {
   try {
+    await setPersistence(auth, rememberMe ? browserLocalPersistence : browserSessionPersistence);
     const result = await signInWithEmailAndPassword(auth, email, password)
     const user = result.user;
     console.log(`Signed in user: ${user.displayName || user.email}`);
     toast.success("Successfully signed in!")
-    return { success: true, user };
+    return { user };
   } catch (error: any) {
     toast.error(`Failed to sign in: ${error.message}`);
-    return {
-      success: false,
-      error: error.message,
-    };
+    return { error: error.message };
+  }
+}
+
+export async function createAccount(email: string, password: string, rememberMe: boolean): Promise<AuthResponse> {
+  try {
+    await setPersistence(auth, rememberMe ? browserLocalPersistence : browserSessionPersistence);
+    const result = await createUserWithEmailAndPassword(auth, email, password)
+    const user = result.user;
+    console.log(`Signed in user: ${user.displayName || user.email}`);
+    toast.success("Successfully signed in!")
+    return { user };
+  } catch (error: any) {
+    console.log(error)
+    toast.error(`Failed to sign up: ${error.message}`);
+    return { error: error.message };
+  }
+}
+
+export async function resetPassword(email: string) {
+  try {
+    if (!email) {
+      return { error: "Missing email" };
+    }
+    await sendPasswordResetEmail(auth, email)
+    toast.success("Password reset email sent, if account exists.")
+    return { error: null }
+  } catch (error: any) {
+    console.log(error)
+    toast.error(`Failed to reset password: ${error.message}`);
+    return { error: error.message };
   }
 }
