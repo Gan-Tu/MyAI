9e960a8f09f7ebf2f70684149323e6011214920e
Gan Tu <dev@tugan.io>
2024-12-31 12:39:49 -0500

    fix cors for webhook

--- next.config.js
+++ next.config.js
@@ -1,8 +1,29 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   // Config options here
+  async headers() {
+    return [
+      {
+        source: "/api/stripe/credits/webhook",
+        headers: [
+          {
+            key: "Access-Control-Allow-Origin",
+            value: "*", // or "https://your-allowed-domain.com"
+          },
+          {
+            key: "Access-Control-Allow-Methods",
+            value: "POST, HEAD, OPTIONS",
+          },
+          {
+            key: "Access-Control-Allow-Headers",
+            value: "Content-Type, Authorization",
+          },
+        ],
+      },
+    ];
+  },
 };
- 
-const withVercelToolbar = require('@vercel/toolbar/plugins/next')();
+
+const withVercelToolbar = require("@vercel/toolbar/plugins/next")();
 // Instead of module.exports = nextConfig, do this:
 module.exports = withVercelToolbar(nextConfig);
